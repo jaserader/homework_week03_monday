e3b86a11b1640638d5754cce9c0d7a1724da9420
Jase Rader <jaserader@me.com>
2015-10-19 14:11:55 -0500

    complete

--- functions.js
+++ functions.js
@@ -25,21 +25,34 @@
 // parameters and returns the sum of those 2 numbers.
 
 
+var sum = function(a, b) {
+  var z = a + b;
+  console.log(z);
+}
 
+sum(2,5)
 
 // Q2
 // Write a function named `avg` that takes 3 parameters
 // and returns the average of those 3 numbers.
 
+var avg = function (a, b, c) {
+  var z = (a + b + c)/ 3;
+  console.log(z);
+}
 
-
+avg(2, 4, 6)
 
 // Q3
 // Write a function called `getLength` that takes one
 // parameter (a string) and returns the length
 
+var getLength = function(a){
+    var z = a.length;
+    console.log (z);
+}
 
-
+getLength("jase rader")
 
 // Q4
 // Write a function called `greaterThan` that takes
@@ -47,8 +60,15 @@
 // parameter is greater than the first. Otherwise
 // the function should return `false`.
 
+var greaterThan = function(a, b) {
+  if (a < b) {
+    console.log (true);
+  } else {
+    console.log(false);
+  }
+}
 
-
+greaterThan(4, 5)
 
 // Q5
 // Write a function called `greet` that takes a
@@ -56,8 +76,11 @@
 // is formated like "Hello, Name!" where *Name*
 // is the parameter that was passed in.
 
+var great = function(name) {
+  console.log("Hello " + name + "!");
+}
 
-
+great("Jase")
 
 // Q6
 // Write a function called `madlib` that takes
@@ -70,8 +93,10 @@
 // words: "quick", "fox", "fence"
 // sentence: "quick brown fox jumps over the fence"
 
+var madLib = function(a, b, c, d) {
+  console.log("The " + a + " green sea " + b + " crawls " + c + " to the deep dark " + d +'.')
+}
 
-
-
+madLib("tiny", "turtle", "slowy", "ocean")
 ///////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////
